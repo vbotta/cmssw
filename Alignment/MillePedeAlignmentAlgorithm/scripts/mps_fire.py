5335e362bc061d50ef705a45b88b1260bed707c5
Martin Descher <martin.descher@cern.ch>
2016-02-10 15:49:43 +0100

    Small fix in mps_fire.py to make forcemerge work if there exist multiple merge-jobs.

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_fire.py
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_fire.py
@@ -169,8 +169,8 @@ else:
 				if not os.path.isfile(backupCfgPath):
 					os.system('cp -p '+cfgPath+' '+backupCfgPath)
 				
-				# rewrite the mergeCfg using only 'OK' jobs
-				inCfgPath = theJobData+'/'+lib.JOBDIR[-1]+'/the.py'
+				# rewrite the mergeCfg using only 'OK' jobs (uses last mille-job as baseconfig)
+				inCfgPath = theJobData+'/'+lib.JOBDIR[lib.nJobs]+'/the.py'
 				command ='mps_merge_v2.py -c '+inCfgPath+' '+Path+'/'+mergeCfg+' '+Path+' '+str(lib.nJobs)
 				os.system(command)
 				
